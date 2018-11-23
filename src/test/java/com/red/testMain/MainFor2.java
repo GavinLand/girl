bf5289113fe1a1ae0947728b84a1a5076dc46197
GavinLand <1311839986@qq.com>
2018-11-23 16:17:34 +0800

    test2

---
+++ src/test/java/com/red/testMain/MainFor2.java
@@ -0,0 +1,13 @@
+package com.red.testMain;
+
+/**
+ * Created by acer on 2018/1/20.
+ */
+public class MainFor2 {
+    public String test(){
+        return "";
+    //
+        //12321
+    }
+	
+}
