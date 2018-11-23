bbac686cb2d9658f30643495c1be89bf0f23f32c
GavinLand <1311839986@qq.com>
2018-11-23 16:55:20 +0800

    test2

---
+++ src/test/java/com/red/testMain/TestA.java
@@ -0,0 +1,9 @@
+package com.red.testMain;
+
+import com.red.GirlService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TestA {
+    @Autowired
+    GirlService girlService;
+}
